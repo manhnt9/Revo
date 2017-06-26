7f6980cb79082f8dbc72b5f16517b2dd2b7ee6d2
ManhNT <zer0.x@outlook.com>
2017-06-26 14:54:01 +0700

    Update login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import { Button, Checkbox, Form } from 'semantic-ui-react';
+
+const inputStyle = {
+  width: 200
+};
 
 const Login = () => (
   <div>
-    <h2>Login page</h2>
+    <br />
+    <Form>
+      <Form.Field>
+        <input placeholder="Username" style={inputStyle} />
+      </Form.Field>
+      <Form.Field>
+        <input type="password" placeholder="Password" style={inputStyle} />
+      </Form.Field>
+      <Form.Field>
+        <Checkbox label="Remember me" />
+      </Form.Field>
+      <Button type="submit">Submit</Button>
+    </Form>
   </div>
 );
 
