6e1fac12aa21abd52bb3d894ea6bb35eddb37d29
ManhNT <zer0.x@outlook.com>
2017-06-30 11:33:17 +0700

    Update login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,22 +5,34 @@ const inputStyle = {
   width: 200
 };
 
-const Login = () => (
-  <div>
-    <Form>
-      <Form.Field>
-        <input placeholder="Username" style={inputStyle} />
-      </Form.Field>
-      <Form.Field>
-        <input type="password" placeholder="Password" style={inputStyle} />
-      </Form.Field>
-      <Form.Field>
-        <Checkbox label="Remember me" />
-      </Form.Field>
-      <Button type="submit">Submit</Button>
-    </Form>
-  </div>
-);
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <Form>
+          <Form.Field>
+            <input placeholder="Username" style={inputStyle} />
+          </Form.Field>
+          <Form.Field>
+            <input type="password" placeholder="Password" style={inputStyle} />
+          </Form.Field>
+          <Form.Field>
+            <Checkbox label="Remember me" />
+          </Form.Field>
+          <Button type="submit">Submit</Button>
+        </Form>
+      </div>
+    );
+  }
+}
 
 export default Login;
 
