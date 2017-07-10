befb71b19c4ea33096e4d4aa64d42af9f50297a2
ManhNT <zer0.x@outlook.com>
2017-07-10 11:47:21 +0700

    Submit form via API

--- src/components/Register.js
+++ src/components/Register.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Button, Form } from 'semantic-ui-react';
 import DatePicker from 'react-datepicker';
 import moment from 'moment';
+import request from 'request';
+import { API_URL } from '../Api.js';
 
 const inputStyle = {
   width: 250
@@ -12,8 +14,8 @@ class Register extends React.Component {
     super(props);
 
     this.state = {
-      date: moment(),
-      name: '', parent_name: '', phone: '', email: '', social_id: '', course: '', note: ''
+      name: '', parent_name: '', dob: moment(), phone: '',
+      email: '', social: '', course: '', note: ''
     };
   }
 
@@ -22,7 +24,22 @@ class Register extends React.Component {
   }
 
   handleSubmit() {
+    const formData = this.state;
+    formData.dob = formData.dob.format('MM/DD/YYYY');
 
+    const options = {
+      url: `${API_URL}/form`,
+      method: 'POST',
+      json: true,
+      body: formData
+    };
+    request(options, (err, res, body) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(body);
+      }
+    });
   }
 
   render() {
@@ -32,49 +49,49 @@ class Register extends React.Component {
           <Form.Field>
             <label>Name</label>
             <Form.Input
-              style={inputStyle} name="name"
+              style={inputStyle} name="name" required
               onChange={(e, { name, value }) => { this.handleChange(e, { name, value }); }}
             />
           </Form.Field>
           <Form.Field>
             <label>Parent name</label>
             <Form.Input
-              style={inputStyle} name="parent_name"
+              style={inputStyle} name="parent_name" required
               onChange={(e, { name, value }) => { this.handleChange(e, { name, value }); }}
             />
           </Form.Field>
           <Form.Field>
             <label>Date of birth</label>
             <DatePicker
-              selected={this.state.date}
-              onChange={(d) => { this.setState({ date: d }); }}
+              selected={this.state.dob}
+              onChange={(d) => { this.setState({ dob: d }); }}
             />
           </Form.Field>
           <Form.Field>
             <label>Phone number</label>
             <Form.Input
-              style={inputStyle} name="phone"
+              style={inputStyle} name="phone" required
               onChange={(e, { name, value }) => { this.handleChange(e, { name, value }); }}
             />
           </Form.Field>
           <Form.Field>
             <label>Email</label>
             <Form.Input
-              style={inputStyle} name="email"
+              style={inputStyle} name="email" required
               onChange={(e, { name, value }) => { this.handleChange(e, { name, value }); }}
             />
           </Form.Field>
           <Form.Field>
             <label>Facebook link or Skype ID</label>
             <Form.Input
-              style={inputStyle} name="social_id"
+              style={inputStyle} name="social" required
               onChange={(e, { name, value }) => { this.handleChange(e, { name, value }); }}
             />
           </Form.Field>
           <Form.Field>
             <label>Course</label>
             <Form.Input
-              style={inputStyle} name="course"
+              style={inputStyle} name="course" required
               onChange={(e, { name, value }) => { this.handleChange(e, { name, value }); }}
             />
           </Form.Field>
