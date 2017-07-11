35a0351bc37aa4aa02fc8e53f3212d7020e16bc1
ManhNT <zer0.x@outlook.com>
2017-07-11 20:06:25 +0700

    Get fee status via API

--- src/components/Fee.js
+++ src/components/Fee.js
@@ -1,27 +1,32 @@
 import React from 'react';
 import { Form, Grid, Checkbox, Table } from 'semantic-ui-react';
+import request from 'request';
+import { API_URL } from '../Api.js';
 
 class Fee extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      currentFilter: 2,
-      students: [
-        {
-          id: 1,
-          name: 'Nguyen Van A',
-          course: 'E1',
-          hasPaid: true
-        },
-        {
-          id: 2,
-          name: 'Nguyen Van B',
-          course: 'E1',
-          hasPaid: false
-        }
-      ]
+      currentFilter: 0,
+      students: []
     };
+  }
+
+  componentWillMount() {
+    const options = {
+      url: `${API_URL}/fee?token=${sessionStorage.token}`,
+      method: 'GET',
+      json: true
+    };
+
+    request(options, (err, res, body) => {
+      if (err) {
+        console.log(err);
+      } else {
+        this.setState({ students: body });
+      }
+    });
   }
 
   setFilter(filter) {
@@ -32,7 +37,7 @@ class Fee extends React.Component {
     const newStudents = this.state.students;
     for (let i = 0; i < newStudents.length; i++) {
       if (newStudents[i].id === id) {
-        newStudents[i].hasPaid = !newStudents[i].hasPaid;
+        newStudents[i].has_paid = !newStudents[i].has_paid;
       }
     }
     this.setState({ students: newStudents });
@@ -45,12 +50,12 @@ class Fee extends React.Component {
         students.push(
           <Table.Row>
             <Table.Cell>{i + 1}</Table.Cell>
-            <Table.Cell>{this.state.students[i].name}</Table.Cell>
+            <Table.Cell>{this.state.students[i].student}</Table.Cell>
             <Table.Cell>{this.state.students[i].course}</Table.Cell>
             <Table.Cell>
               <Checkbox
                 onClick={() => { this.handlePaymentChange(this.state.students[i].id); }}
-                checked={this.state.students[i].hasPaid}
+                checked={this.state.students[i].has_paid}
               />
             </Table.Cell>
           </Table.Row>
@@ -58,16 +63,16 @@ class Fee extends React.Component {
       }
     } else if (this.state.currentFilter === 1) {
       for (let i = 0; i < this.state.students.length; i++) {
-        if (this.state.students[i].hasPaid === true) continue;
+        if (this.state.students[i].has_paid === true) continue;
         students.push(
           <Table.Row>
             <Table.Cell>{i + 1}</Table.Cell>
-            <Table.Cell>{this.state.students[i].name}</Table.Cell>
+            <Table.Cell>{this.state.students[i].student}</Table.Cell>
             <Table.Cell>{this.state.students[i].course}</Table.Cell>
             <Table.Cell>
               <Checkbox
                 onClick={() => { this.handlePaymentChange(this.state.students[i].id); }}
-                checked={this.state.students[i].hasPaid}
+                checked={this.state.students[i].has_paid}
               />
             </Table.Cell>
           </Table.Row>
@@ -75,16 +80,16 @@ class Fee extends React.Component {
       }
     } else if (this.state.currentFilter === 2) {
       for (let i = 0; i < this.state.students.length; i++) {
-        if (this.state.students[i].hasPaid === false) continue;
+        if (this.state.students[i].has_paid === false) continue;
         students.push(
           <Table.Row>
             <Table.Cell>{i + 1}</Table.Cell>
-            <Table.Cell>{this.state.students[i].name}</Table.Cell>
+            <Table.Cell>{this.state.students[i].student}</Table.Cell>
             <Table.Cell>{this.state.students[i].course}</Table.Cell>
             <Table.Cell>
               <Checkbox
                 onClick={() => { this.handlePaymentChange(this.state.students[i].id); }}
-                checked={this.state.students[i].hasPaid}
+                checked={this.state.students[i].has_paid}
               />
             </Table.Cell>
           </Table.Row>
@@ -94,25 +99,25 @@ class Fee extends React.Component {
 
     return (
       <div>
-        <div courseName="ui one column stackable center aligned page grid">
-          <div courseName="column">
+        <div coursestudent="ui one column stackable center aligned page grid">
+          <div coursestudent="column">
             <Form.Group grouped>
               <Grid>
                 <Grid.Row>
                   <Grid.Column width={5}>
-                    <Form.Field label="All" control="input" type="radio" name="htmlRadios"
+                    <Form.Field label="All" control="input" type="radio" student="htmlRadios"
                       onClick={() => { this.setFilter(0); }}
                       checked={this.state.currentFilter === 0}
                     />
                   </Grid.Column>
                   <Grid.Column width={5}>
-                    <Form.Field label="Hasn't paid" control="input" type="radio" name="htmlRadios"
+                    <Form.Field label="Hasn't paid" control="input" type="radio" student="htmlRadios"
                       onClick={() => { this.setFilter(1); }}
                       checked={this.state.currentFilter === 1}
                     />
                   </Grid.Column>
                   <Grid.Column width={5}>
-                    <Form.Field label="Has paid" control="input" type="radio" name="htmlRadios"
+                    <Form.Field label="Has paid" control="input" type="radio" student="htmlRadios"
                       onClick={() => { this.setFilter(2); }}
                       checked={this.state.currentFilter === 2}
                     />
@@ -126,7 +131,7 @@ class Fee extends React.Component {
           <Table.Header>
             <Table.Row>
               <Table.HeaderCell>No</Table.HeaderCell>
-              <Table.HeaderCell>Name</Table.HeaderCell>
+              <Table.HeaderCell>Student</Table.HeaderCell>
               <Table.HeaderCell>Course</Table.HeaderCell>
               <Table.HeaderCell>Payment</Table.HeaderCell>
             </Table.Row>
